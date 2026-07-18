e1698917cde20ee520da1adb7850a189009fdafc
Susovon123 <150047799+Susovon123@users.noreply.github.com>
2026-07-18 18:54:18 +0530

    Updated project files

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+
+
+const menuBtn = document.getElementById("menuBtn");
+const closeBtn = document.getElementById("closeBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+const overlay = document.getElementById("overlay");
+
+menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.remove("right-[-100%]");
+    mobileMenu.classList.add("right-0");
+
+    overlay.classList.remove("hidden");
+});
+
+closeBtn.addEventListener("click", closeMenu);
+overlay.addEventListener("click", closeMenu);
+
+function closeMenu() {
+    mobileMenu.classList.remove("right-0");
+    mobileMenu.classList.add("right-[-100%]");
+
+    overlay.classList.add("hidden");
+}  
+
 // Signup Modal starts
 
 const signupModal = document.getElementById("signupModal");
@@ -33,8 +57,6 @@ if (skipSignup) {
     });
 
 }
-
-
 // Click Outside Modal Close
 if (signupModal) {
 
@@ -54,52 +76,14 @@ if (signupModal) {
 
 // Signup Modal ends
 
-const menuBtn = document.getElementById("menuBtn");
-const closeBtn = document.getElementById("closeBtn");
-const mobileMenu = document.getElementById("mobileMenu");
-const overlay = document.getElementById("overlay");
+document.getElementById("signinLink").addEventListener("click", function (e) {
+    e.preventDefault();
 
-menuBtn.addEventListener("click", () => {
-    mobileMenu.classList.remove("right-[-100%]");
-    mobileMenu.classList.add("right-0");
-
-    overlay.classList.remove("hidden");
+    document.getElementById("signupModal").classList.add("hidden");
+    document.getElementById("loginModal").classList.remove("hidden");
 });
-
-closeBtn.addEventListener("click", closeMenu);
-overlay.addEventListener("click", closeMenu);
-
-function closeMenu() {
-    mobileMenu.classList.remove("right-0");
-    mobileMenu.classList.add("right-[-100%]");
-
-    overlay.classList.add("hidden");
-}  
-
-
-// modal
-// document.addEventListener("DOMContentLoaded", function () {
-
-//     const openBtn = document.getElementById("openModal");
-//     const modal = document.getElementById("loginModal");
-
-//     if (!openBtn || !modal) {
-//         console.log("Element not found");
-//         return;
-//     }
-
-//     openBtn.addEventListener("click", function(e) {
-//         e.preventDefault();
-
-//         modal.classList.remove("hidden");
-//         modal.classList.add("flex");
-//     });
-
-//     modal.addEventListener("click", function(e) {
-//         if (e.target === modal) {
-//             modal.classList.remove("flex");
-//             modal.classList.add("hidden");
-//         }
-//     });
-
-// });
+document.querySelectorAll(".closeModal").forEach(btn => {
+    btn.addEventListener("click", function () {
+        this.closest(".fixed").classList.add("hidden");
+    });
+});
